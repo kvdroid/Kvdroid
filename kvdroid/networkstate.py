a494f79c7f9299da96752d9b28c0034c4d40a42f
Akubue-Izundu Kenechukwu <kengoon19@gmail.com>
2021-03-30 16:23:29 +0100

    Create networkstate.py

---
+++ kvdroid/networkstate.py
@@ -0,0 +1,17 @@
+from kvdroid import ConnectivityManager
+
+
+def network_state():
+    conn = con_mgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnectedOrConnecting()
+    try:
+        if conn:
+            return True
+        else:
+            conn = con_mgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnectedOrConnecting()
+            if conn:
+                return True
+            else:
+                return False
+    except:
+         return False
+network_state = network_state()
