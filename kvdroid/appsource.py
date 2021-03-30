8748a0ec2376c83b7984ff2c4e98ddd2748fdf1d
Akubue-Izundu Kenechukwu <kengoon19@gmail.com>
2021-03-30 16:19:53 +0100

    Create appsource.py

---
+++ kvdroid/appsource.py
@@ -0,0 +1,11 @@
+from kvdroid import activity
+
+
+def app_source():
+    packageName = activity.getPackageName()
+    installer =  activity.getPackageManager().getInstallerPackageName(packageName)
+    if installer == "com.android.vending":
+        return "playstore"
+    else:
+        return "unknown"
+app_source = app_source()
