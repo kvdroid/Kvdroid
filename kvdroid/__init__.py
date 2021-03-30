0be8bf8500c7c68a89ecf345eb022bde58d9537c
Akubue-Izundu Kenechukwu <kengoon19@gmail.com>
2021-03-30 16:14:21 +0100

    Update __init__.py

--- kvdroid/__init__.py
+++ kvdroid/__init__.py
@@ -34,11 +34,146 @@ if platform == "android":
         StrictMode = autoclass('android.os.StrictMode')
         MediaPlayer = autoclass('android.media.MediaPlayer')
         AudioManager = autoclass('android.media.AudioManager')
+        rctx = autoclass('android.graphics.Rect')()
+        currentActivity = cast('android.app.Activity', activity)
+        parcelable = cast('android.os.Parcelable', uri)
+        context = cast('android.content.Context', activity.getApplicationContext())	
+        dm = cast("android.app.DownloadManager",activity.getSystemService(Context.DOWNLOAD_SERVICE))
         
     except BaseException:
         Logger.error(
             "Kvdroid: Cannot load classes by Pyjnius. Make sure requirements installed"
         )
+        
+    def keyboard_height():
+        try:
+            decor_view = activity.getWindow().getDecorView()
+            height = activity.getWindowManager().getDefaultDisplay().getHeight()
+            decor_view.getWindowVisibleDisplayFrame(rctx)
+            return height - rctx.bottom
+        except:
+            return 0
+        
+    def device_info(yaz):
+        os = {
+            'model': Build.MODEL,
+            'brand': Build.BRAND,
+            'manufacturer': Build.MANUFACTURER,
+            'version': VERSION.RELEASE,
+            'sdk': VERSION.SDK,
+            'product': Build.PRODUCT,
+            'base': VERSION.BASE_OS,
+            'rom': VERSION.INCREMENTAL,
+            'security': VERSION.SECURITY_PATCH,
+            'hardware': Build.HARDWARE,
+            'tags': Build.TAGS,
+            'sdk_int': VERSION.SDK_INT
+        }
+        return os[yaz]
+    
+    @run_on_ui_thread
+    def immersive_mode(immerse = None):
+        if not immerse:
+            pass
+        else:
+            if immerse == True:
+            	window = activity.getWindow()
+            	return window.getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            else:
+                window = activity.getWindow()
+                return window.getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_VISIBLE)
+            
+    def launch_app(app_package, app_activity):
+        intent = Intent()
+        intent.setAction(Intent.ACTION_VIEW)    
+        intent.setClassName(app_package, app_activity)
+        return activity.startActivity(intent)
+    
+    def app_details(app_package):
+        intent = Intent()
+        intent.setAction(Provider.ACTION_APPLICATION_DETAILS_SETTINGS)    
+        uri = Uri.parse("package:"+app_package)           
+        intent.setData(uri)
+        activity.startActivity(intent)
+        
+    @run_on_ui_thread
+    def statusbar_color(color,text_color):
+    	window = activity.getWindow()
+    	if str(text_color) == "black":
+    	    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+    	elif str(text_color) == "white":
+    	    window.getDecorView().setSystemUiVisibility(0)
+    	else:
+    	    raise  TypeError("Available options are ['white','black'] for StatusBar text color")
+    	window.clearFlags(WindowManager.FLAG_TRANSLUCENT_STATUS)
+    	window.addFlags(WindowManager.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+    	window.setStatusBarColor(Color.parseColor(str(color)))
+        
+    @run_on_ui_thread
+    def navbar_color(color):
+        window = activity.getWindow()
+        window.setNavigationBarColor(Color.parseColor(str(color)))
+        
+    def toast(message):
+        return PythonActivity.toastError(str(message))
+    
+    def set_wallpaper(path_to_image):
+        file = File(str(path_to_image))	
+        bitmap = BitmapFactory.decodeFile(file.getAbsolutePath())	
+        manager = WallpaperManager.getInstance(context)
+        return manager.setBitmap(bitmap)
+
+    def speech(text,lang):
+        tts = TextToSpeech(activity, None)
+        tts.setLanguage(Locale(str(lang)))
+        return tts.speak(str(text), TextToSpeech.QUEUE_FLUSH, None)
+    
+    def download_manager(title,description,url,folder,file_name):
+        request = Request(uri)
+        request.setTitle(str(title))
+        request.setDescription(str(description))
+        request.setNotificationVisibility(Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        request.setDestinationInExternalPublicDir(str(folder), str(file_name))
+        dm.enqueue(request)
+
+    def restart_app(restart=False):
+        if not restart:
+            pass
+        else:
+            intent = Intent()
+            packageManager = context.getPackageManager()
+            intent = packageManager.getLaunchIntentForPackage(context.getPackageName())
+            componentName = intent.getComponent()
+            mainIntent = Intent.makeRestartActivityTask(componentName)
+            context.startActivity(mainIntent)
+            Runtime.getRuntime().exit(0)
+       
+    def share_text(text):
+        intent = Intent()
+        intent.setAction(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_TEXT, String(str(text)))
+        intent.setType("text/plain")
+        chooser = Intent.createChooser(intent, String('Share'))
+        activity.startActivity(chooser)
+   
+    def share_file(text):
+        path = str(text)
+        StrictMode.disableDeathOnFileUriExposure()  
+        shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("*/*")    
+        imageFile = File(path)
+        uri = Uri.fromFile(imageFile)
+        shareIntent.putExtra(Intent.EXTRA_STREAM, parcelable)
+        currentActivity.startActivity(shareIntent)
+
+
 else:
     Logger.error(
         "Kvdroid: Kvdroid is only callable for Android"
