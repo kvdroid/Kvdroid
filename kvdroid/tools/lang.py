e3702228f051bfd59f66232ab05299ac0dd2f3dc
Yunus Ceyhan <yunus.ceyhn@gmail.com>
2022-01-03 08:17:51 -0800

    Added option to get device language or country in a specific language

--- kvdroid/tools/lang.py
+++ kvdroid/tools/lang.py
@@ -1,20 +1,19 @@
 from kvdroid.jclass.java.util import Locale
 
-
-def device_lang(option = "Language"):
+def device_lang(option = "Language", display_lang = None):
     locale = Locale().getDefault()
     options = {
         "Language" : locale.getLanguage() , 
         "ISO3Language" : locale.getISO3Language() ,
         "Country" : locale.getCountry() ,
         "ISO3Country" : locale.getISO3Country() ,
-        "DisplayCountry" : locale.getDisplayCountry() ,
-        "DisplayName" : locale.getDisplayName() ,
+        "DisplayCountry" : locale.getDisplayCountry(Locale( str(display_lang) )) ,
+        "DisplayName" : locale.getDisplayName(Locale( str(display_lang) )) ,
         "String" : locale.toString() ,
-        "DisplayLanguage" : locale.getDisplayLanguage() ,
+        "DisplayLanguage" : locale.getDisplayLanguage(Locale( str(display_lang) )) ,
         "LanguageTag" : locale.toLanguageTag() }
 
     if option not in options.keys():
         raise ValueError(f"Invalid option. Expected one of: {list(options.keys())}")
     else:
-        return options[str(option)]
+        return options[option]
