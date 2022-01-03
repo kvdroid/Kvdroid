f59a2db19d72243c047e401cdf9a8ef4f9221b22
Yunus Ceyhan <yunus.ceyhn@gmail.com>
2022-01-03 04:24:11 -0800

    Added new options to device_lang

--- kvdroid/tools/lang.py
+++ kvdroid/tools/lang.py
@@ -1,5 +1,20 @@
 from kvdroid.jclass.java.util import Locale
 
 
-def device_lang():
-    return Locale().getDefault().getLanguage()
+def device_lang(option = "Language"):
+    locale = Locale().getDefault()
+    options = {
+        "Language" : locale.getLanguage() , 
+        "ISO3Language" : locale.getISO3Language() ,
+        "Country" : locale.getCountry() ,
+        "ISO3Country" : locale.getISO3Country() ,
+        "DisplayCountry" : locale.getDisplayCountry() ,
+        "DisplayName" : locale.getDisplayName() ,
+        "String" : locale.toString() ,
+        "DisplayLanguage" : locale.getDisplayLanguage() ,
+        "LanguageTag" : locale.toLanguageTag() }
+
+    if option not in options.keys():
+        raise ValueError(f"Invalid option. Expected one of: {list(options.keys())}")
+    else:
+        return options[str(option)]
