ead205de60d144f3f089f158814949b02d00a879
Akubue Kenechukwu <kengoon19@gmail.com>
2022-01-03 17:14:45 +0100

    reformatted code to suit python PEP

--- kvdroid/tools/lang.py
+++ kvdroid/tools/lang.py
@@ -1,18 +1,18 @@
 from kvdroid.jclass.java.util import Locale
 
 
-def device_lang(option = "Language"):
+def device_lang(option="Language"):
     locale = Locale().getDefault()
     options = {
-        "Language" : locale.getLanguage() , 
-        "ISO3Language" : locale.getISO3Language() ,
-        "Country" : locale.getCountry() ,
-        "ISO3Country" : locale.getISO3Country() ,
-        "DisplayCountry" : locale.getDisplayCountry() ,
-        "DisplayName" : locale.getDisplayName() ,
-        "String" : locale.toString() ,
-        "DisplayLanguage" : locale.getDisplayLanguage() ,
-        "LanguageTag" : locale.toLanguageTag() }
+        "Language": locale.getLanguage(),
+        "ISO3Language": locale.getISO3Language(),
+        "Country": locale.getCountry(),
+        "ISO3Country": locale.getISO3Country(),
+        "DisplayCountry": locale.getDisplayCountry(),
+        "DisplayName": locale.getDisplayName(),
+        "String": locale.toString(),
+        "DisplayLanguage": locale.getDisplayLanguage(),
+        "LanguageTag": locale.toLanguageTag()}
 
     if option not in options.keys():
         raise ValueError(f"Invalid option. Expected one of: {list(options.keys())}")
