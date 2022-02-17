99b2c4a4ed98fa5a8e282bb2c3202d93ba06acea
Yunus Ceyhan <yunus.ceyhn@gmail.com>
2022-02-17 02:41:01 -0800

    added more class..

--- kvdroid/jclass/android/graphics.py
+++ kvdroid/jclass/android/graphics.py
@@ -5,14 +5,29 @@ from kvdroid.jclass import _class_call
 def Color(*args, instantiate: bool = False):
     return _class_call(autoclass("android.graphics.Color"), args, instantiate)
 
-
 def Rect(*args, instantiate: bool = False):
     return _class_call(autoclass('android.graphics.Rect'), args, instantiate)
 
+def Bitmap(*args, instantiate: bool = False):
+    return _class_call(autoclass('android.graphics.Bitmap'), args, instantiate)
+    
+def BitmapDrawable(*args, instantiate: bool = False):
+    return _class_call(autoclass('android.graphics.drawable.BitmapDrawable'), args, instantiate)
 
 def BitmapFactory(*args, instantiate: bool = False):
     return _class_call(autoclass('android.graphics.BitmapFactory'), args, instantiate)
-
+    
+def Config(*args, instantiate: bool = False):
+    return _class_call(autoclass("android.graphics.Bitmap$Config"), args, instantiate)
+    
+def CompressFormat(*args, instantiate: bool = False):
+    return _class_call(autoclass("android.graphics.Bitmap$CompressFormat"), args, instantiate)
 
 def Point(*args, instantiate: bool = False):
     return _class_call(autoclass("android.graphics.Point"), args, instantiate)
+    
+def Drawable(*args, instantiate: bool = False):
+    return _class_call(autoclass("android.graphics.drawable.Drawable"), args, instantiate)
+    
+def Dimen(*args, instantiate: bool = False):
+    return _class_call(autoclass("android.R$dimen"), args, instantiate)  
