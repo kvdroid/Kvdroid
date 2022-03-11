f512b8273a58770a59341d6b90b4047f680bbe99
kengoon <kengoon19@gmail.com>
2022-03-11 22:02:43 +0100

    Fixed PEP 8: E302 expected 2 blank lines

--- kvdroid/jclass/android/graphics.py
+++ kvdroid/jclass/android/graphics.py
@@ -1,39 +1,50 @@
-from jnius import autoclass # NOQA
+from jnius import autoclass  # NOQA
 from kvdroid.jclass import _class_call
 
 
 def Canvas(*args, instantiate: bool = False):
     return _class_call(autoclass("android.graphics.Canvas"), args, instantiate)
 
+
 def Color(*args, instantiate: bool = False):
     return _class_call(autoclass("android.graphics.Color"), args, instantiate)
+
 
 def Rect(*args, instantiate: bool = False):
     return _class_call(autoclass('android.graphics.Rect'), args, instantiate)
 
+
 def Bitmap(*args, instantiate: bool = False):
     return _class_call(autoclass('android.graphics.Bitmap'), args, instantiate)
-    
+
+
 def BitmapDrawable(*args, instantiate: bool = False):
     return _class_call(autoclass('android.graphics.drawable.BitmapDrawable'), args, instantiate)
-    
+
+
 def AdaptiveIconDrawable(*args, instantiate: bool = False):
     return _class_call(autoclass('android.graphics.drawable.AdaptiveIconDrawable'), args, instantiate)
-    
+
+
 def BitmapFactory(*args, instantiate: bool = False):
     return _class_call(autoclass('android.graphics.BitmapFactory'), args, instantiate)
-    
+
+
 def Config(*args, instantiate: bool = False):
     return _class_call(autoclass("android.graphics.Bitmap$Config"), args, instantiate)
-    
+
+
 def CompressFormat(*args, instantiate: bool = False):
     return _class_call(autoclass("android.graphics.Bitmap$CompressFormat"), args, instantiate)
 
+
 def Point(*args, instantiate: bool = False):
     return _class_call(autoclass("android.graphics.Point"), args, instantiate)
-    
+
+
 def Drawable(*args, instantiate: bool = False):
     return _class_call(autoclass("android.graphics.drawable.Drawable"), args, instantiate)
-    
+
+
 def Dimen(*args, instantiate: bool = False):
-    return _class_call(autoclass("android.R$dimen"), args, instantiate)  
+    return _class_call(autoclass("android.R$dimen"), args, instantiate)
