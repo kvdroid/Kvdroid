f959db8ae65fccaa1b412dbce2ca7dcbc9d199c3
kengoon <kengoon19@gmail.com>
2022-04-24 16:33:55 +0100

    added `RelativeLayout`, `LinearLayout`, `TextView` class

--- kvdroid/jclass/android/widget.py
+++ kvdroid/jclass/android/widget.py
@@ -2,5 +2,17 @@ from jnius import autoclass
 from kvdroid.jclass import _class_call
 
 
-def Toast(instantiate: bool=False, *args):
+def Toast(*args, instantiate: bool = False):
     return _class_call(autoclass('android.widget.Toast'), args, instantiate)
+
+
+def RelativeLayout(*args, instantiate: bool = False):
+    return _class_call(autoclass('android.widget.RelativeLayout'), args, instantiate)
+
+
+def LinearLayout(*args, instantiate: bool = False):
+    return _class_call(autoclass('android.widget.LinearLayout'), args, instantiate)
+
+
+def TextView(*args, instantiate: bool = False):
+    return _class_call(autoclass('android.widget.TextView'), args, instantiate)
