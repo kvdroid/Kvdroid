7887649ab86f92ccc02ed8a7350cb59f4d2fcc39
kengoon <kengoon19@gmail.com>
2022-04-24 16:35:24 +0100

    created `EventLoop` for polling events and holding the UI

---
+++ kvdroid/base.py
@@ -0,0 +1,40 @@
+from kvdroid.event import EventDispatcher
+from kvdroid import activity
+from kvdroid.app import App
+
+
+class EventLoop(EventDispatcher):
+    def __init__(self):
+        super(EventLoop, self).__init__()
+        self.app = App.get_running_app()
+        self.quit = False
+        self.status = "idle"
+        self.resume = False
+        self.destroyed = False
+        self.paused = False
+
+    def mainloop(self):
+        while not self.quit and self.status == "created":
+            self.poll()
+
+    def poll(self):
+        if activity.isResumed() and not self.resume:
+            self.app.dispatch("on_resume")
+            self.resume = activity.resume
+            self.paused = False
+
+        if activity.isDestroyed() and not self.destroyed:
+            self.app.dispatch("on_destroy")
+            self.destroyed = activity.destroyed
+            self.resume = False
+
+        if not activity.hasWindowFocus() and not self.paused:
+            self.app.dispatch("on_pause")
+            self.paused = True
+            self.resume = False
+
+    def close(self):
+        self.quit = True
+        self.status = "destroyed"
+
+
