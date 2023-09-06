8596b54b65ba67b7d47748ae2e5524674207ad28
Kenechukwu Akubue <kengoon19@gmail.com>
2023-09-06 14:39:13 +0100

    simplified `create_notification` arguments and broke down the algorithm into separate functions

--- kvdroid/tools/notification.py
+++ kvdroid/tools/notification.py
@@ -31,57 +31,59 @@ try:
         PendingIntent,
         NotificationManager,
         Notification,
+        NotificationChannel,
         VERSION
     )
     from kvdroid.jclass.java.lang import String, System
 
 
-    def create_notification(
-            small_icon: Union[int, str, InputStream()], channel_id: str, title: str,
-            text: str, ids: int, channel_name: str,
-            large_icon: Union[int, str, InputStream()] = 0, big_picture: Union[int, str, InputStream()] = 0,
-            action_title1: str = "", action_title2: str = "", action_title3: str = "", key_text_reply: str = "",
-            reply_title: str = "",
-            expandable: bool = False, set_large_icon: bool = False, add_action_button2: bool = False,
-            add_action_button3: bool = False, auto_cancel: bool = True, add_action_button1: bool = False,
-            set_reply: bool = False, put_extra: bool = False, extras: tuple = ("test", "test"),
-            small_icon_color: int = 0
-    ):  # sourcery no-metrics
-        intent = Intent(activity.getApplication().getApplicationContext(), python_act)
-        if put_extra:
+    def _create_intent(extras, java_class):
+        intent = Intent(activity.getApplication().getApplicationContext(), java_class)
+        if extras:
             intent.putExtra(*extras)
         intent.setFlags(Intent().FLAG_ACTIVITY_CLEAR_TOP | Intent().FLAG_ACTIVITY_SINGLE_TOP)
         intent.setAction(Intent().ACTION_MAIN)
         intent.addCategory(Intent().CATEGORY_LAUNCHER)
-        if VERSION().SDK_INT >= 31:
-            pendingIntent = PendingIntent().getActivity(
-                activity.getApplication().getApplicationContext(), 0, intent,
-                PendingIntent().FLAG_MUTABLE
+        return (
+            PendingIntent().getActivity(
+                activity.getApplication().getApplicationContext(),
+                0,
+                intent,
+                PendingIntent().FLAG_MUTABLE,
             )
-        else:
-            pendingIntent = PendingIntent().getActivity(
-                activity.getApplication().getApplicationContext(), 0, intent,
-                PendingIntent().FLAG_UPDATE_CURRENT | Notification().FLAG_AUTO_CANCEL
+            if VERSION().SDK_INT >= 31
+            else PendingIntent().getActivity(
+                activity.getApplication().getApplicationContext(),
+                0,
+                intent,
+                PendingIntent().FLAG_UPDATE_CURRENT
+                | Notification().FLAG_AUTO_CANCEL,
             )
-        # Create the NotificationChannel, but only on API 26+ because
-        # the NotificationChannel class is new and not in the support library
-        notificationManager = cast(NotificationManager(), activity.getSystemService(
-            Context().NOTIFICATION_SERVICE
-        ))
-        if VERSION().SDK_INT >= 26:
-            from kvdroid.jclass.android.app import NotificationChannel
-            importance: int = NotificationManager().IMPORTANCE_HIGH
-            channel = NotificationChannel(String(channel_id), String(channel_name), importance)
-            channel.setDescription("notification")
-            # Register the channel with the system; you can't change the importance
-            # or other notification behaviors after this
-            notificationManager.createNotificationChannel(channel)
+        )
 
-        builder = NotificationCompatBuilder(activity.getApplication().getApplicationContext(), channel_id)
-        if isinstance(small_icon, int):
-            builder.setSmallIcon(small_icon)
-        else:
-            builder.setSmallIcon(bitmap_to_drawable(small_icon))
+
+    def _create_notification_channel(channel_id, channel_name, notification_manager):
+        importance: int = NotificationManager().IMPORTANCE_HIGH
+        channel = NotificationChannel(String(channel_id), String(channel_name), importance)
+        channel.setDescription("notification")
+        # Register the channel with the system; you can't change the importance
+        # or other notification behaviors after this
+        notification_manager.createNotificationChannel(channel)
+
+
+    def _build_notification_content(
+            auto_cancel,
+            channel_id,
+            pending_intent,
+            small_icon,
+            small_icon_color,
+            text,
+            title,
+            priority,
+            defaults,
+            builder
+    ):
+        builder.setSmallIcon(small_icon)
         if small_icon_color:
             try:
                 builder.setColor(ContextCompat().getColor(activity.getApplicationContext(), small_icon_color))
@@ -90,59 +92,122 @@ try:
         builder.setContentTitle(String(title))
         builder.setContentText(String(text))
         # Set the intent that will fire when the user taps the notification
-        builder.setContentIntent(pendingIntent)
+        builder.setContentIntent(pending_intent)
         builder.setWhen(System().currentTimeMillis())
         builder.setChannelId(String(channel_id))
-        builder.setPriority(NotificationCompat().PRIORITY_HIGH)
-        builder.setDefaults(NotificationCompat().DEFAULT_ALL)
+        builder.setPriority(priority)
+        builder.setDefaults(defaults)
         builder.setAutoCancel(auto_cancel)
         builder.setTicker(String(""))
         builder.setContentInfo("INFO")
         builder.setStyle(NotificationBigTextStyle(instantiate=True).bigText(String(text)).setBigContentTitle(text))
-        if add_action_button1:
+
+    def _build_notification_actions(
+            action_title1,
+            action_title2,
+            action_title3,
+            auto_cancel,
+            key_text_reply,
+            builder,
+            reply_title,
+            pending_intent,
+    ):
+        if action_title1:
             builder.addAction(
-                get_resource("mipmap").icon, cast_object("charSequence", String(action_title1)), pendingIntent
+                get_resource("mipmap").icon, cast_object("charSequence", String(action_title1)), pending_intent
             )
 
-        if add_action_button2:
+        if action_title2:
             builder.addAction(
-                get_resource("mipmap").icon, cast_object("charSequence", String(action_title2)), pendingIntent
+                get_resource("mipmap").icon, cast_object("charSequence", String(action_title2)), pending_intent
             )
 
-        if add_action_button3:
+        if action_title3:
             builder.addAction(
-                get_resource("mipmap").icon, cast_object("charSequence", String(action_title3)), pendingIntent
+                get_resource("mipmap").icon, cast_object("charSequence", String(action_title3)), pending_intent
             )
 
-        if set_reply:
+        if reply_title:
             builder.addAction(
                 NotificationCompatActionBuilder(
-                    get_resource("mipmap").icon, String(reply_title), pendingIntent).addRemoteInput(
-                    RemoteInputBuilder(String(key_text_reply)).setLabel(
-                        String(reply_title)).build()
+                    get_resource("mipmap").icon,
+                    String(reply_title),
+                    pending_intent
+                ).addRemoteInput(
+                    RemoteInputBuilder(String(key_text_reply)).setLabel(String(reply_title)).build()
                 ).build()
             ).setAutoCancel(auto_cancel)
 
-        if expandable:
-            largePicture = NotificationCompatBigPictureStyle(instantiate=True)
-            if isinstance(big_picture, int):
-                largePicture.bigPicture(BitmapFactory.decodeResource(activity.getResources(), big_picture))
-            elif isinstance(big_picture, str):
-                largePicture.bigPicture(BitmapFactory.decodeFile(big_picture))
-            else:
-                largePicture.bigPicture(BitmapFactory.decodeStream(big_picture))
-            builder.setStyle(largePicture)
+    def _set_big_picture(big_picture, builder):
+        large_picture = NotificationCompatBigPictureStyle(instantiate=True)
+        if isinstance(big_picture, int):
+            large_picture.bigPicture(BitmapFactory.decodeResource(activity.getResources(), big_picture))
+        elif isinstance(big_picture, str):
+            large_picture.bigPicture(BitmapFactory.decodeFile(big_picture))
+        else:
+            large_picture.bigPicture(BitmapFactory.decodeStream(big_picture))
+        builder.setStyle(large_picture)
 
-        if set_large_icon:
-            if isinstance(large_icon, int):
-                builder.setLargeIcon(BitmapFactory.decodeResource(activity.getResources(), large_icon))
-            elif isinstance(large_icon, str):
-                builder.setLargeIcon(BitmapFactory.decodeFile(large_icon))
-            else:
-                builder.setLargeIcon(BitmapFactory.decodeStream(large_icon))
+    def _set_large_icon(large_icon, builder):
+        if isinstance(large_icon, int):
+            builder.setLargeIcon(BitmapFactory.decodeResource(activity.getResources(), large_icon))
+        elif isinstance(large_icon, str):
+            builder.setLargeIcon(BitmapFactory.decodeFile(large_icon))
+        else:
+            builder.setLargeIcon(BitmapFactory.decodeStream(large_icon))
+
+
+    def create_notification(
+            small_icon: int,
+            channel_id: str,
+            title: str,
+            text: str,
+            ids: int,
+            channel_name: str,
+            large_icon: Union[int, str, InputStream()] = 0,
+            big_picture: Union[int, str, InputStream()] = 0,
+            action_title1: str = None,
+            action_title2: str = None,
+            action_title3: str = None,
+            key_text_reply: str = None,
+            reply_title: str = None,
+            auto_cancel: bool = True,
+            extras: tuple = None,
+            small_icon_color: int = 0,
+            java_class=python_act,
+            priority: int = None,
+            defaults: int = None
+    ):  # sourcery no-metrics
+        if not priority:
+            priority = NotificationCompat().PRIORITY_HIGH
+        if not defaults:
+            defaults = NotificationCompat().DEFAULT_ALL
+        # Create the NotificationChannel, but only on API 26+ because
+        # the NotificationChannel class is new and not in the support library
+        notification_manager = cast(NotificationManager(), activity.getSystemService(
+            Context().NOTIFICATION_SERVICE
+        ))
+        if VERSION().SDK_INT >= 26:
+            _create_notification_channel(channel_id, channel_name, notification_manager)
+        pending_intent = _create_intent(extras=extras, java_class=java_class)
+        builder = NotificationCompatBuilder(activity.getApplication().getApplicationContext(), channel_id)
+        _build_notification_content(
+            auto_cancel, channel_id, pending_intent, small_icon, small_icon_color, text, title, priority, defaults,
+            builder
+        )
+        _build_notification_actions(
+            action_title1, action_title2, action_title3, auto_cancel, key_text_reply, builder, reply_title,
+            pending_intent
+        )
+
+        if big_picture:
+            _set_big_picture(big_picture, builder)
+
+        if large_icon:
+            _set_large_icon(large_icon, builder)
 
         # notificationId is a unique int for each notification that you must define
-        notificationManager.notify(ids, builder.build())
+        notification_manager.notify(ids, builder.build())
 except JavaException as error:
     Logger.error(
         f"{str(error)}\n"
