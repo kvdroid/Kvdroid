c3e42b169493a8ede3bc30a6d07dbe4d6594f1e7
Kenechukwu Akubue <kengoon19@gmail.com>
2023-09-06 22:06:52 +0100

    further simplified function extractions and separated intent actions for different notification action

--- kvdroid/tools/notification.py
+++ kvdroid/tools/notification.py
@@ -8,12 +8,9 @@ from jnius import cast, JavaException  # NOQA
 from android import python_act  # NOQA
 from kvdroid import Logger, activity
 from kvdroid.cast import cast_object
-from kvdroid.jclass.android import BitmapFactory
 from kvdroid.jclass.java import InputStream
 from kvdroid.tools import get_resource
-from kvdroid.tools.graphics import bitmap_to_drawable
 
-BitmapFactory = BitmapFactory()
 
 try:
     from kvdroid.jclass.androidx import (
@@ -32,6 +29,7 @@ try:
         NotificationManager,
         Notification,
         NotificationChannel,
+        BitmapFactory,
         VERSION
     )
     from kvdroid.jclass.java.lang import String, System
@@ -100,7 +98,7 @@ try:
         builder.setAutoCancel(auto_cancel)
         builder.setTicker(String(""))
         builder.setContentInfo("INFO")
-        builder.setStyle(NotificationBigTextStyle(instantiate=True).bigText(String(text)).setBigContentTitle(text))
+        builder.setStyle(NotificationBigTextStyle(instantiate=True).bigText(String(text)).setBigContentTitle(title))
 
     def _build_notification_actions(
             action_title1,
@@ -109,22 +107,29 @@ try:
             auto_cancel,
             key_text_reply,
             builder,
+            extras,
+            java_class,
             reply_title,
-            pending_intent,
     ):
-        if action_title1:
+        if extra := action_title1 and extras.get("action1"):
             builder.addAction(
-                get_resource("mipmap").icon, cast_object("charSequence", String(action_title1)), pending_intent
+                get_resource("mipmap").icon,
+                cast_object("charSequence", String(action_title1)),
+                _create_intent(extras=extra, java_class=java_class)
             )
 
-        if action_title2:
+        if extra := action_title2 and extras.get("action2"):
             builder.addAction(
-                get_resource("mipmap").icon, cast_object("charSequence", String(action_title2)), pending_intent
+                get_resource("mipmap").icon,
+                cast_object("charSequence", String(action_title2)),
+                _create_intent(extras=extra, java_class=java_class)
             )
 
-        if action_title3:
+        if extra := action_title3 and extras.get("action3"):
             builder.addAction(
-                get_resource("mipmap").icon, cast_object("charSequence", String(action_title3)), pending_intent
+                get_resource("mipmap").icon,
+                cast_object("charSequence", String(action_title3)),
+                _create_intent(extras=extra, java_class=java_class)
             )
 
         if reply_title:
@@ -132,7 +137,7 @@ try:
                 NotificationCompatActionBuilder(
                     get_resource("mipmap").icon,
                     String(reply_title),
-                    pending_intent
+                    _create_intent(extras=extra, java_class=java_class)
                 ).addRemoteInput(
                     RemoteInputBuilder(String(key_text_reply)).setLabel(String(reply_title)).build()
                 ).build()
@@ -141,20 +146,20 @@ try:
     def _set_big_picture(big_picture, builder):
         large_picture = NotificationCompatBigPictureStyle(instantiate=True)
         if isinstance(big_picture, int):
-            large_picture.bigPicture(BitmapFactory.decodeResource(activity.getResources(), big_picture))
+            large_picture.bigPicture(BitmapFactory().decodeResource(activity.getResources(), big_picture))
         elif isinstance(big_picture, str):
-            large_picture.bigPicture(BitmapFactory.decodeFile(big_picture))
+            large_picture.bigPicture(BitmapFactory().decodeFile(big_picture))
         else:
-            large_picture.bigPicture(BitmapFactory.decodeStream(big_picture))
+            large_picture.bigPicture(BitmapFactory().decodeStream(big_picture))
         builder.setStyle(large_picture)
 
     def _set_large_icon(large_icon, builder):
         if isinstance(large_icon, int):
-            builder.setLargeIcon(BitmapFactory.decodeResource(activity.getResources(), large_icon))
+            builder.setLargeIcon(BitmapFactory().decodeResource(activity.getResources(), large_icon))
         elif isinstance(large_icon, str):
-            builder.setLargeIcon(BitmapFactory.decodeFile(large_icon))
+            builder.setLargeIcon(BitmapFactory().decodeFile(large_icon))
         else:
-            builder.setLargeIcon(BitmapFactory.decodeStream(large_icon))
+            builder.setLargeIcon(BitmapFactory().decodeStream(large_icon))
 
 
     def create_notification(
@@ -164,17 +169,17 @@ try:
             text: str,
             ids: int,
             channel_name: str,
-            large_icon: Union[int, str, InputStream()] = 0,
-            big_picture: Union[int, str, InputStream()] = 0,
+            large_icon: Union[int, str, InputStream()] = None,
+            big_picture: Union[int, str, InputStream()] = None,
             action_title1: str = None,
             action_title2: str = None,
             action_title3: str = None,
             key_text_reply: str = None,
             reply_title: str = None,
             auto_cancel: bool = True,
-            extras: tuple = None,
-            small_icon_color: int = 0,
-            java_class=python_act,
+            extras: dict = None,
+            small_icon_color: int = None,
+            java_class: object = None,
             priority: int = None,
             defaults: int = None
     ):  # sourcery no-metrics
@@ -182,22 +187,18 @@ try:
             priority = NotificationCompat().PRIORITY_HIGH
         if not defaults:
             defaults = NotificationCompat().DEFAULT_ALL
-        # Create the NotificationChannel, but only on API 26+ because
-        # the NotificationChannel class is new and not in the support library
-        notification_manager = cast(NotificationManager(), activity.getSystemService(
-            Context().NOTIFICATION_SERVICE
-        ))
-        if VERSION().SDK_INT >= 26:
-            _create_notification_channel(channel_id, channel_name, notification_manager)
-        pending_intent = _create_intent(extras=extras, java_class=java_class)
+        if java_class:
+            java_class = python_act
+
+        tap_pending_intent = _create_intent(extras=extras.get("tap"), java_class=java_class)
         builder = NotificationCompatBuilder(activity.getApplication().getApplicationContext(), channel_id)
         _build_notification_content(
-            auto_cancel, channel_id, pending_intent, small_icon, small_icon_color, text, title, priority, defaults,
+            auto_cancel, channel_id, tap_pending_intent, small_icon, small_icon_color, text, title, priority, defaults,
             builder
         )
         _build_notification_actions(
-            action_title1, action_title2, action_title3, auto_cancel, key_text_reply, builder, reply_title,
-            pending_intent
+            action_title1, action_title2, action_title3, auto_cancel, key_text_reply, builder, extras, java_class,
+            reply_title,
         )
 
         if big_picture:
@@ -206,6 +207,13 @@ try:
         if large_icon:
             _set_large_icon(large_icon, builder)
 
+        notification_manager = cast(NotificationManager(), activity.getSystemService(
+            Context().NOTIFICATION_SERVICE
+        ))
+        # Create the NotificationChannel, but only on API 26+ because
+        # the NotificationChannel class is new and not in the support library
+        if VERSION().SDK_INT >= 26:
+            _create_notification_channel(channel_id, channel_name, notification_manager)
         # notificationId is a unique int for each notification that you must define
         notification_manager.notify(ids, builder.build())
 except JavaException as error:
