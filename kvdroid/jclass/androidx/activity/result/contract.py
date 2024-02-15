81a67f56efe64cfd0dc6df319c285ca7e7b809c8
Kenechukwu Akubue <kengoon19@gmail.com>
2024-02-15 17:58:45 +0100

    Add PickMultipleVisualMedia Android API

--- kvdroid/jclass/androidx/activity/result/contract.py
+++ kvdroid/jclass/androidx/activity/result/contract.py
@@ -8,3 +8,11 @@ def PickVisualMedia(*args, instantiate: bool = False):
         args,
         instantiate
     )
+
+
+def PickMultipleVisualMedia(*args, instantiate: bool = False):
+    return _class_call(
+        autoclass("androidx.activity.result.contract.ActivityResultContracts$PickMultipleVisualMedia"),
+        args,
+        instantiate
+    )
